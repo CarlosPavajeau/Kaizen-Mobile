3d1da4a294fb127366852ca62695912939d56e2c
Cantte <cantte098@gmail.com>
2020-10-27 10:14:54 -0500

    refactor: Move MainActivity file

--- app/src/main/java/com/kaizen/view/ui/activities/MainActivity.kt
+++ app/src/main/java/com/kaizen/view/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.kaizen
+package com.kaizen.view.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kaizen.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
